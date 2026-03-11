4c874b4bb219e7aee0bf94f5e34a5d3fdf1b4b6c
junios-study <junios@naver.com>
2026-03-11 14:51:23 +0900

    문자열 처리 함수 만들어 보기

--- main.cpp
+++ main.cpp
@@ -1,35 +1,25 @@
 #include <iostream>
+#include "StringUtil.h"
+
 
 using namespace std;
 
+
+
+
+
 int main()
 {
-	int Number[5] = { 1, 2, 3, 4, 5 };
-	char Hello[6] = { 'H','e', '\0','l', 'l', 'o' };
-	//Number[0];
+	int A = 10;
+	char Temp[100] = "Hello World";
 
-	Hello[0] = 'J';
+	int StringLength = Length(Temp, 100);
+	cout << StringLength << endl;
 
-	cout << Number[1] << endl;
+	Replace(Temp, 100, 'l', 'f');
 
-	int* P = Number;
-	char* C = Hello;
-
-	Hello[3] = 'A';
-	//cout << *(P + 1) << endl;
-	//cout << *(Number+1) << endl;
-	//cout << *(C + 0) << endl;
-	Console.WriteLine << C << endl;
-	for (int i = 0; i < 6; i++)
-	{
-		if (C[i] == '\0')
-		{
-			break;
-		}
-		cout << C[i];
-	}
-
-
+	cout << Temp << endl;
 
 	return 0;
 }
+
