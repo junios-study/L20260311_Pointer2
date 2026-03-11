5edd8e89c95d20056025e788f5c3451032899e14
junios-study <junios@naver.com>
2026-03-11 16:49:57 +0900

    동적 생성

--- main.cpp
+++ main.cpp
@@ -6,6 +6,10 @@ using namespace std;
 
 int main()
 {
+	malloc(10);
+
+	free();
+	//compnter memory unmanaged language / human managed language
 	//heap 동적으로 int 사이즈 만들고 주소 반환
 	int* MyTexture = new int;
 
@@ -19,13 +23,15 @@ int main()
 
 	int Size = 100;
 	//heap 동적으로 int 100개짜리 배열 사이즈 만들고 주소 반환
-	int* MyStaticMesh = new int[Size];
+	int* MyStaticMeshes = new int[Size];
 
-	MyStaticMesh[3] = 10;
-	//포인터 변수의 주소의 heap 영역 반환
-	delete MyStaticMesh;
-	MyStaticMesh = nullptr;
-	if (MyStaticMesh)
+	MyStaticMeshes[3] = 10;
+
+	//포인터 변수의 주소 부터 할당 받은 배열 크기 만큼의 heap 영역 반환
+	//delete MyStaticMeshes; 첫번째만 반환하고 나머지는 잊어버림
+	delete[] MyStaticMeshes;
+	MyStaticMeshes = nullptr;
+	if (MyStaticMeshes)
 	{
 
 	}
